6e0f75c904de57bdb95012035be4efe702bd2245
Daryl Lukas <me@daryllukas.co>
2016-02-08 15:52:35 +0200

    Added ServiceProvider

---
+++ src/SugarAPIServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Daryl\SugarAPI;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+
+class SugarAPIServiceProvider extends ServiceProvider {
+
+	/**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/config.php' => config_path('l5-sugarcrm.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/config.php', 'l5-sugarcrm'
+        );
+
+        App::bind('sugarcrm', function() {
+            return new LaravelReeve;
+        });
+
+    }
+}
